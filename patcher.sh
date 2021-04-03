c93fb30b8570b87f1abd18e12cb43745d7eae79a
Yunyu Lin <mail@yunyul.in>
2021-04-03 10:12:59 -0400

    OxygenOS 11

--- patcher.sh
+++ patcher.sh
@@ -6,13 +6,13 @@ echo "Decompiling"
 java -jar ../bin/apktool.jar d ../bin/net.oneplus.widget.apk
 cd net.oneplus.widget
 echo "Patching"
-find ./smali/net/oneplus/widget -type f -exec sed -i 's/\"1\"/\"\"/' {} \;
-sed -i 's/android\:sharedUserId\=\"android.uid.system\"//' AndroidManifest.xml
-sed -i "s/versionCode\: '/versionCode\: '999/" apktool.yml
-sed -i "s/versionName\: /versionName\: patched-/" apktool.yml
-# sed -i 's/android\:updatePeriodMillis\=\"0\"/android\:updatePeriodMillis\=\"1800000\"/' res/xml/weather_widget_provider.xml
-# sed -i 's/package=\"net.oneplus.widget\"/package=\"net.oneplus.widget_onecolor\"/' AndroidManifest.xml
-cp ../../preview.png ./res/drawable/
+find ./smali/net/oneplus/widget -type f -exec sed -i '' -e 's/\"1\"/\"\"/' {} \;
+sed -i '' -e 's/android\:sharedUserId\=\"android.uid.system\"//' AndroidManifest.xml
+sed -i '' -e "s/versionCode\: '/versionCode\: '999/" apktool.yml
+sed -i '' -e "s/versionName\: /versionName\: patched-/" apktool.yml
+# sed -i '' -e 's/android\:updatePeriodMillis\=\"0\"/android\:updatePeriodMillis\=\"1800000\"/' res/xml/weather_widget_provider.xml
+# sed -i '' -e s/package=\"net.oneplus.widget\"/package=\"net.oneplus.widget_onecolor\"/' AndroidManifest.xml
+cp ../../weather_preview.png ./res/drawable/
 echo "Recompiling"
 cd ..
 java -jar ../bin/apktool.jar b ./net.oneplus.widget
